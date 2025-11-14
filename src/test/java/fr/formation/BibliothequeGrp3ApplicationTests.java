d2891ab5e6b34582ffa1108b90415ce97b608165
Thibaud Reigner <thibaud.reigner@gmail.com>
2025-11-14 12:40:29 +0100

    ajout du projet Java

---
+++ src/test/java/fr/formation/BibliothequeGrp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeGrp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
